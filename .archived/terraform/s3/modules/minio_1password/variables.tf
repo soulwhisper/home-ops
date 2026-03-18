b3cfc93c2e242bd35ed86c4dd193ac4aaea74132
Hekatos Noir <soulwhisper@outlook.com>
2026-03-18 10:05:45 +0800

    feat(app): add authentik, cleanup archives

--- .archived/terraform/s3/modules/minio_1password/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vault" {
-  description = "Vault name to find the Item in"
-  type        = string
-}
-
-variable "item" {
-  description = "Password Item to fetch"
-  type        = string
-}
--- .archived/terraform/s3/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "minio" {
-  alias  = "nas"
-  minio_server   = module.onepassword_item_minio.fields.server
-  minio_user     = module.onepassword_item_minio.fields.username
-  minio_password = module.onepassword_item_minio.fields.password
-  minio_ssl      = false
-}
